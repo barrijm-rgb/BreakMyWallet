3d64bbf99c98b7294d7caa8a71122913cf6ee158
barrijm-rgb <barrijm@gmail.com>
2025-10-07 13:46:43 -0500

    Create bmw_maint_tracker.py

---
+++ bmw_maint_tracker.py
@@ -0,0 +1,18 @@
+BreakMyWallet/
+│
+├─ bmw_maint_tracker.py
+├─ requirements.txt
+├─ README.md
+├─ NOTES-CONFIGURE.md
+├─ .gitignore
+│
+├─ .github/workflows/build.yml
+│
+├─ installer/BreakMyWalletInstaller.iss
+│
+└─ assets/logos/
+     ├─ logo_2008_m3.svg
+     ├─ logo_2013_335i.svg
+     ├─ logo_2020_x7.svg
+     ├─ logo_2021_x3.svg
+     └─ icon.svg
